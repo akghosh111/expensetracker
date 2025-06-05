f21434f4c7e77ab4cffb2878ac432df45422161b
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-05 20:53:01 +0530

    feat: added tanstack router - navbar

--- backend/frontend/src/routes/index.tsx
+++ backend/frontend/src/routes/index.tsx
@@ -1,3 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+
+
+
+
 
 import {
   Card,
@@ -9,7 +15,12 @@ import {
 
 import { useQuery } from "@tanstack/react-query";
 
-import { api } from "./lib/api";
+import { api } from "@/lib/api";
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
 
 async function getTotalSpent() {
   const res = await api.expenses["total-spent"].$get()
@@ -20,7 +31,7 @@ async function getTotalSpent() {
   return data
 }
 
-function App() {
+function Index() {
     const { isPending, error, data } = useQuery({ queryKey: ['get-total-spent'], queryFn: getTotalSpent })
 
 
@@ -47,4 +58,3 @@ function App() {
   )
 }
 
-export default App
