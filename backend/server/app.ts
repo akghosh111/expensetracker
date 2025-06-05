86c30a694e1ef7f2c6408b59c136dd6f8335c811
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-05 19:33:06 +0530

    refactor: hono rpc, ssg(removed)

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -7,10 +7,8 @@ const app = new Hono()
 app.use(logger())
 
 
-app.get("/test", c => {
-    return c.json({"message": "test"})
-})
+const apiRoutes= app.basePath("/api").route("/expenses", expenseRoute)
 
-app.route("/api/expenses", expenseRoute)
+export default app;
 
-export default app
+export type ApiRoutes = typeof apiRoutes
